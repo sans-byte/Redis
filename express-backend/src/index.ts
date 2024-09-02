6eb92caa35cd193caa462213ee80aedebafb9df4
sans-byte <sanskarj15@gmail.com>
2024-09-02 18:11:21 +0530

    first commit

---
+++ express-backend/src/index.ts
@@ -0,0 +1,36 @@
+import express from "express";
+import { createClient } from "redis";
+
+const app = express();
+
+const client = createClient();
+client.on("error", console.error);
+
+app.post("/submit", async (req, res) => {
+  const { problemId, code, language } = req.body;
+  try {
+    await client.lPush(
+      "problems",
+      JSON.stringify({ code, language, problemId })
+    );
+    res.status(200).send("Submission recienved and stored");
+  } catch (err) {
+    console.error("Redis error", err);
+    return res.status(500).send("Failed to store submission");
+  }
+});
+
+async function startServer() {
+  try {
+    await client.connect();
+    console.log("Connected to redis client");
+
+    app.listen(3000, () => {
+      console.log("Connected to server");
+    });
+  } catch (error) {
+    console.log("Failed to connect to redis or server", error);
+  }
+}
+
+startServer();
