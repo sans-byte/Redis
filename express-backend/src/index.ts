250b27487ddeeca99d8cde31eef7b9113e4cdf09
sans-byte <sanskarj15@gmail.com>
2024-09-02 19:18:49 +0530

    changed some configs

--- express-backend/src/index.ts
+++ express-backend/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { createClient } from "redis";
 
 const app = express();
+app.use(express.json());
 
 const client = createClient();
 client.on("error", console.error);
@@ -10,10 +11,10 @@ app.post("/submit", async (req, res) => {
   const { problemId, code, language } = req.body;
   try {
     await client.lPush(
-      "problems",
+      "submissions",
       JSON.stringify({ code, language, problemId })
     );
-    res.status(200).send("Submission recienved and stored");
+    res.status(200).send("Submission recieved and stored");
   } catch (err) {
     console.error("Redis error", err);
     return res.status(500).send("Failed to store submission");
